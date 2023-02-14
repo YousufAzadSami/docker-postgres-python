68bb51906e26cd4a98bf092edeab19eec66828fb
Sami <sami1592@gmail.com>
2023-02-14 18:17:02 +0100

    cosmetic change, try catch

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # What is this project?
-This is meant to be a barebone minimal project, that demonstrates how data from a Postgres container can be accessed+CRUD operations can be run from another container using python. 
+This project demonstrates how data from a Postgres container can be accessed+CRUD operations can be run from another container using python. This was done as an interview task. This is meant to be a barebone minimal project, i.e. the codes here are kept simple and are for experimental purpose. In no shape or form, they are production-ready code. 
 
 Container 1: Postgres container (preloaded with data using .sql files)  
-Container 2: Python container  
+Container 2: Python container (accesses data from the first container)
 
 # How to run this project 
 Prerequisites: Docker needs to be installed in the local machine 
-- Open terminal in the root directory of this repo and run the command `docker compose up`
-- Upon running the command, the Postgres container and Python container will start. The python container will split out outputs in command line. This output can be seen either in the terminal or in Docker Hub inside the python container > output tab. 
-- To stop the containers run the command `docker compose down -v`. The `-v` will make the volumes are deleted. This will come in handy, when new databases/tables needs to be initialized via .sql files. 
+- Clone or download the `repo` 
+- Open terminal in the root directory of this `repo`. Then run the command `docker compose up` (tested from CMD and PowerShell). Add `--build` if you wish to force rebuild. This comes helpful when there is a change in the code after the first build. 
+- Upon running the command - 
+  - The Postgres container and Python container will start as instructed in the `docker-compose.yml` file
+  - The Postgres container has some data (`customer` table in `postgres_super_db` database) preloaded into it from the `customers.sql` file. 
+  - The python container will wait 5 seconds so that the Postgres container can start up first. I used ``sleep()`` function to wait manually. This is a adhoc solution for developtment puposes only. 
+  - The python container will connect to the database, read and split out outputs in command line. This output can be seen either in the terminal or in Docker Hub inside the python container > output tab. 
+- To stop the containers run the command `docker compose down`. Adding `-v` will delete the volumes. This will come in handy, when new databases/tables needs to be initialized via .sql files. 
 
 # Rooms for improvemnts 
 
 This project was made in a hurry, needless to say there are many rooms for improvents. Some of them are - 
+- Instead of manually waiting for the Postgres container to start, find some alternative solution. 
 - Use `sqlachemy` instead of `psycopg2`. The reason being, `sqlalchemy` offers much more pythonic way of handling sql queries instead of handling raw sql queries, which can get more difficult to manange as the query gets more complicated. 
--- python_stuff/app.py
+++ python_stuff/app.py
@@ -4,21 +4,21 @@ import datetime as dt
 
 
 def select_all(cursor):
-    print(f'\nSelecting and printing all rows from the table "customers"\n')
+    print(f'Selecting and printing all rows from the table "customers" - \n')
     sql_query = f"SELECT * FROM customers;"
     # print(sql_query)
     cursor.execute(sql_query)
     sql_rows = cursor.fetchall()
     print_rows(sql_rows)
 
-    print(f'\nRow count: {cursor.rowcount}\n')
+    print(f'Row count: {cursor.rowcount}\n')
 
 def print_rows(rows):
     for row in rows:
         print(row)
 
 def delete_older_dates(cursor, date_threshold = dt.date(2023, 2, 23)):
-    print(f'\nDeleting records that are older than {date_threshold}')
+    print(f'Deleting records that are older than {date_threshold}\n')
     sql_query = "DELETE FROM customers where (customer_date < %s);"
     # print(sql_query)
     cursor.execute(sql_query, (date_threshold, ))
@@ -42,39 +42,51 @@ def select_condition(cursor):
     cursor.execute(sql_query, (date_2, ))
     print_rows(sql_rows)
 
+def wait_a_while():
+    for x in range(5):
+        print(f'Waiting manually for 5 seconds so that the Postgres container can start first : {5-x}s left') 
+        time.sleep(1)
+    print('Waited manually (via time.sleep()) for 5 seconds so that the Postgres container can start first. Not the best solution, but it will do for now\n')
+
 if __name__ == '__main__':
 
-    print('\nManually waiting 5s (via time.sleep()) for the Postgres container to start. Not the best solution, but it will do for now.\n')
-    for x in range(5):
-        print(f'Waiting for 5 seconds : {5-x}s left') 
-        time.sleep(1)
+    # waiting a while so that the Postgres container can start first
+    wait_a_while()
 
-    #establishing the connection
-    conn = psycopg2.connect(
-        database="postgres_super_db", 
-        user='postgres_user', 
-        password='postgres_pass', 
-        host='db', 
-        port= '5432'
-    )
-    #Creating a cursor object using the cursor() method
-    cursor = conn.cursor()
+    # for the sake of simplicity, I am putting the whole connection and sql query part in the inside of try
+    try:
+        #establishing the connection
+        conn = psycopg2.connect(
+            database="postgres_super_db", 
+            user='postgres_user', 
+            password='postgres_pass', 
+            host='db', 
+            port= '5432'
+        )
+        #Creating a cursor object using the cursor() method
+        cursor = conn.cursor()
+        cursor.execute('SELECT version()')
+        print(f'Connection established to {cursor.fetchone()}')
+        print(f'\nThe Postgres container is preloaded with a "customers" table in the "postgres_super_db" database from customers.sql file. It has 5 records\n')
 
-    # select and print everything from the 'customer' table in the database
-    select_all(cursor)
+        # select and print everything from the 'customer' table in the database
+        select_all(cursor)
 
-    # select and print some stuff depending on the condition from the 'customer' table in the database
-    # select_condition(cursor)
+        # select and print some stuff depending on the condition from the 'customer' table in the database
+        # select_condition(cursor)
 
-    # delete all the records that are older than a certain date
-    delete_older_dates(cursor)
-    # or
-    # delete_older_dates(cursor, dt.date(2023, 2, 24))
+        # delete all the records that are older than a certain date
+        delete_older_dates(cursor)
+        # or
+        # delete_older_dates(cursor, dt.date(2023, 2, 24))
 
-    # select and print everything from the 'customer' table in the database
-    select_all(cursor)
+        # select and print everything from the 'customer' table in the database
+        select_all(cursor)
+        
+
+        #Closing the connection
+        print('closing the connection')
+        conn.close()
     
-
-    #Closing the connection
-    print('closing the connection')
-    conn.close()
+    except:
+        print('Something went wrong. Following the print statements will give us a clue')
